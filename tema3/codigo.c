f9317166f794c8f4a375da605fbb948690712224
Miguelo0098 <i62rarum@uco.es>
2018-10-17 10:29:03 +0200

    Añadidos apuntes tema 3

---
+++ tema3/codigo.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double resolverEcuacionpos(double a, double b, double c){
+	double dividendo = -b + sqrt(b*b-4*a*c);
+	double divisor = 2*a;
+	return dividendo/divisor;
+}
+
+double resolverEcuacionneg(double a, double b, double c){
+	double dividendo = -b - sqrt(b*b-4*a*c);
+	double divisor = 2*a;
+	return dividendo/divisor;
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc < 2)
+	{
+		printf("ERROR: no se han introducido los parámetros necesarios\n");
+		exit(-1);
+	}
+
+	double a, b, c;
+	a = atod(argv[1]);
+	b = atod(argv[1]);
+	c = atod(argv[1]);
+
+	printf("ECUACIÓN: %lfx^2 + %lfx + %lf = 0\n", a, b, c);
+	printf("RESULTADOS: \n");
+	printf("A) %lf\n", resolverEcuacionpos(a, b, c));
+	printf("B) %lf\n", resolverEcuacionneg(a, b, c));
+
+
+
+	return 0;
+}
